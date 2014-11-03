d1d468975a4a4902460833b81691908fdf77e5fe
Łukasz Wieczorek <wieczorek1990@gmail.com>
2014-11-03 17:31:29 +0100

    fix

--- uninstall.sh
+++ uninstall.sh
@@ -3,5 +3,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DIR; source config.sh; cd - > /dev/null
 
 sudo rm -rf ${dest}
-sudo rm -f ${bin}/ortografia
-sudo rm -f ${bin}/ortografia_server
+sudo rm -f ${bin}/ort
+sudo rm -f ${bin}/ort_server
